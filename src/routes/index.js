7df01f3a4b6fc481d50a58052794d142ed6c81f4
fpannunzio <fpannunzio@itba.edu.ar>
2022-04-01 23:34:00 -0300

    Pagina de about cableada

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+  
 });
 
 router.get('/about', function(req, res, next) {
