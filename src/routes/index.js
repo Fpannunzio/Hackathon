8ea61f0a9878d175d369046ed1b38cfc81cc2db6
fpannunzio <fpannunzio@itba.edu.ar>
2022-04-02 17:19:41 -0300

    Agrego Uikit y purgo codigo

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,22 +7,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
-    { name: 'Tux', organization: "Linux", birth_year: 1996 },
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
-  ];
-  var tagline = "No programming concept is complete without a cute animal mascot.";
+  // var mascots = [
+  //   { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
+  //   { name: 'Tux', organization: "Linux", birth_year: 1996 },
+  //   { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
+  // ];
+  // var tagline = "No programming concept is complete without a cute animal mascot.";
 
   res.render('index', {
-    mascots: mascots,
-    tagline: tagline
+    // mascots: mascots,
+    // tagline: tagline
   });
 
-});
-
-router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'Express' });
 });
 
 router.get('/qrtest', function (req, res, next) {
@@ -67,19 +63,6 @@ router.post("/uploadCV", async (req, res) => {
     });
   res.render('cv', { file: cv });
 });
-
-router.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null };
-    res.render('db', results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
 
 router.get('/retrieve', async (req, res) => {
   res.render('retrieve', {});
